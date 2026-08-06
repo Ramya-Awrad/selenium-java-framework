0fa6375fc4080cb852daceb7ed7f46234728af7f
Ramya Awrad <ramyaawrad9030@gmail.com>
2026-08-06 20:14:20 +0530

    Day 2: Configure Maven project and execute first TestNG test

---
+++ seleniumjavaframework/src/test/java/com/framework/tests/SampleTest.java
@@ -0,0 +1,13 @@
+package com.framework.tests;
+
+import org.testng.annotations.Test;
+
+public class SampleTest {
+
+    @Test
+    public void firstTest() {
+
+        System.out.println("My First TestNG Test");
+
+    }
+}

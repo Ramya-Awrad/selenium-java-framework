0f9a080493f116feac839ba00909e2a2824335f7
Ramya Awrad <ramyaawrad9030@gmail.com>
2026-08-12 17:42:20 +0530

    Added Log4j2 logging configuration and test logs

--- seleniumjavaframework/src/test/java/com/framework/tests/LoginTest.java
+++ seleniumjavaframework/src/test/java/com/framework/tests/LoginTest.java
@@ -5,19 +5,27 @@ import com.framework.pages.LoginPage;
 
 import com.framework.utils.ConfigReader;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 public class LoginTest extends BaseTest {
+
+    private static final Logger logger =
+        LogManager.getLogger(LoginTest.class);
     
     @Test
     public void loginTest() {
 
+        logger.info("Starting login test");
+
         driver.get(ConfigReader.get("url"));
+        logger.info("Opened SauceDemo application");
 
         LoginPage loginPage = new LoginPage(driver);
-
-        System.out.println(ConfigReader.get("url"));
-        System.out.println(ConfigReader.get("browser"));
+        logger.info("LoginPage object created");
 
         loginPage.login("standard_user", "secret_sauce");
+        logger.info("Login completed successfully");
     }
 
     
